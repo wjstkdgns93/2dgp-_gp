77c4d2cd3b39dbf45dee05305e2da28bb8cf2655
wjstkdgns93 <wjstkdgns93@naver.com>
2015-10-10 16:39:36 +0900

    장애물과 점프구현
    장애물 종류와 캐릭터 1차결정
    점프구현(아직 미구현)

--- character move.py
+++ character move.py
@@ -1,19 +1,50 @@
 from pico2d import *
+
+
+class Character:
+    def __init__(self):
+        self.x,self.y=200,120
+        self.image =load_image('character1.png')
+    def update(self):
+        pass
+    def draw(self):
+        self.image.clip_draw(0, 0, 40, 40,self.x,self.y)
+
+def handle_events():
+
+    global start
+
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            start = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            start = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_x:
+            pass
+
+start = True
+jump=0
+jumpcount=0
+count=0
+
 open_canvas()
 
-character = load_image('movecharcter1.png')
+character= Character()
 
-x = 800
-frame =0;
 
-while(x>0):
+while(start):
+
+    handle_events()
+
+    character.update()
+
     clear_canvas()
-    character.clip_draw(frame*72, 0, 72, 58,x,100)
+    character.draw()
     update_canvas()
-    frame=(frame+1)%3
-    x-=10
-    delay(0.1)
-    get_events()
+
+    delay(0.2)
 
 
 
