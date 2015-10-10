394b8dd5fb6371894fd0d0df0b3c595ff722a204
wjstkdgns93 <wjstkdgns93@naver.com>
2015-10-10 19:22:38 +0900

    점프완성
    지속입력 해결
    점프 완성

--- character move.py
+++ character move.py
@@ -1,14 +1,33 @@
 from pico2d import *
 
 
-class Character:
+class Character():
     def __init__(self):
         self.x,self.y=200,120
+        self.jumpnum=0
+        self.dir=0
+        self.flag=0
         self.image =load_image('character1.png')
+
+    def clickflag(self,flag):
+        self.flag=flag
+    def xclick(self,dir):
+        self.dir=dir
+
     def update(self):
-        pass
+        self.y+=(self.dir * 20)
+        if self.y >= 200:
+            self.dir = -1
+            self.jumpnum=1
+        elif self.y <=120 and self.jumpnum==1:
+            self.jumpnum=0
+            if self.flag==0:
+                self.dir = 0
+            elif self.flag==1:
+                self.dir = 1
     def draw(self):
         self.image.clip_draw(0, 0, 40, 40,self.x,self.y)
+
 
 def handle_events():
 
@@ -21,13 +40,14 @@ def handle_events():
             start = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             start = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_x:
-            pass
+        if event.type == SDL_KEYDOWN and event.key == SDLK_x:
+            character.xclick(1)
+            character.clickflag(1)
+            event.type = SDL_KEYUP
+        elif event.type == SDL_KEYUP and event.key == SDLK_x:
+            character.clickflag(0)
 
 start = True
-jump=0
-jumpcount=0
-count=0
 
 open_canvas()
 
@@ -44,7 +64,7 @@ while(start):
     character.draw()
     update_canvas()
 
-    delay(0.2)
+    delay(0.02)
 
 
 
